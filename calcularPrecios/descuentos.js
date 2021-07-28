60763136068082c4c079e22d4a9fd35cf212a730
Tomás Elizondo <tomelizif@gmail.com>
2021-07-28 16:33:15 -0300

    Fin Taller 2

---
+++ calcularPrecios/descuentos.js
@@ -0,0 +1,19 @@
+function calcularPrecioConDescuento(precio,descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+};
+
+function buttonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue,discountValue);
+
+    const resultP = document.getElementById("priceResult");
+    resultP.innerText = "El precio con descuento es: $" + precioConDescuento;
+}
