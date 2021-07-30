ac8ee8fe1de060a13f5ee0b734548e122e4a6c3c
Tomás Elizondo <tomelizif@gmail.com>
2021-07-29 21:06:37 -0300

    FIN del taller 3

--- promedioMedianaModa/promedio.js
+++ promedioMedianaModa/promedio.js
@@ -7,8 +7,8 @@ function calcularMediaAritmetica(lista){
     
     // .reduce() = metodo de los arrays que ahorra el for de arriba.
     const sumaLista = lista.reduce(
-        function (valorAcumulado = 0, nuevoElemento) {
-            return valorAcumulado + nuevoElemento;
+        function (a = 0, b) {
+            return a + b;
         }
     ); 
 
@@ -18,13 +18,17 @@ function calcularMediaAritmetica(lista){
 
 // MEDIANA
 const lista1 = [
+    700,
+    100,
     200,
     300,
     500,
-    700
+    100,
+    500,
+    100,
 ];
-const mitadLista1 = parseInt(lista1.length / 2);
 
+let listaOrdenada = lista1.sort((a, b) => a - b);
 function esPar(num){
     if(num % 2 === 0){
         return true;
@@ -34,6 +38,7 @@ function esPar(num){
 }
 let mediana;
 function calcularMediana(){
+    const mitadLista1 = parseInt(listaOrdenada.length / 2);
     if(esPar(lista1.length)){
     mediana = (lista1[mitadLista1] + lista1[mitadLista1 - 1]) / 2;
     } else {
@@ -41,3 +46,56 @@ function calcularMediana(){
     }
     return mediana;
 }
+
+// MODA
+const lista2 = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    3,
+    4,
+    5,
+    3,
+    2,
+    1,
+    2,
+    3,
+    4,
+    3,
+    2,
+    1,
+    2,
+    3,
+    2,
+    1,
+];
+
+const lista2Count = {};
+
+lista2.map(
+    function (elemento) {
+        if (lista2Count[elemento]){
+            lista2Count[elemento] += 1;
+        } else {
+            lista2Count[elemento] = 1;   
+        }
+    }
+);
+
+const lista2Array = Object.entries(lista2Count).sort(
+    function (elementoA,elementoB) { 
+        return elementoA[1] - elementoB[1];
+    }
+);
+// es como: .sort((a, b) => a[1] - b[1]);
+// y tambien es como .sort(
+//     function (a,b){
+//         if (a[1] < b[1]) return 1;
+//         if (a[1] == b[1]) return 0;
+//         if (a[1] > b[1]) return -1;
+//     }
+// );
+let moda = lista2Array[lista2Array.length - 1];
